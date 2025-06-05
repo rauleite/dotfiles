66947d6fcf7d7cd3c1422eb4c254f61275f79029
Raul Leite <raul.abreu.leite@gmail.com>
2025-06-05 20:23:06 +0000

    Removendo diretório nvim para adicionar como submódulo

--- nvim/init.lua
+++
@@ -1,6 +0,0 @@
--- o modulo lazy carrega o core.options antes de seu startup
-require("core.lazy")
-require("core.keymaps")
-
--- options pós-Lazy (que dependem de plugins)
-require("core.options2")
--- nvim/lua/core/options2.lua
+++
@@ -1,9 +0,0 @@
---
--- Lazy post-startup options
---
-
-vim.cmd.colorscheme("catppuccin")
-
--- dart requires manual enabling as it is not managed by Mason
--- obs. this config will be done by flutter-tools.nvim plugin
--- vim.lsp.enable({ 'dart' })
