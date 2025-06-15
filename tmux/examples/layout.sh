341a77141d7db58281b0a8d6f52ead3ae63d1f8a
rauleite <raul.abreu.leite@gmail.com>
2025-06-15 17:54:04 -0300

    changes(tmux): removido sensible plugin e alguns sets

--- tmux/examples/layout.sh
+++ tmux/examples/layout.sh
@@ -13,10 +13,17 @@ tmux split-window -h -l 50% zsh
 # sleep 0.2
 
 # 2. Seleciona o painel esquerdo e divide verticalmente (para ter dois painéis na coluna esquerda)
-tmux select-pane -t 0
+tmux select-pane -t 1
 # sleep 0.2
 tmux split-window -v -l 50% "tail -f /var/log/syslog"
 # sleep 0.2
 
 # 3. No painel superior (esquerdo) envia o comando para iniciar o nvim.
-tmux send-keys -t 0 "nvim ." Enter
+tmux send-keys -t 1 "nvim ." Enter
+
+# 4. Foco no painel 1
+tmux select-pane -t 1
+
+
+tmux resize-pane -t 1 -R 15  # Expande 5 células para a direita
+tmux resize-pane -t 1 -D 5  # Expande 5 células para baixo
