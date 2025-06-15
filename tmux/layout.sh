b955325095e8f11c8f4bb8de39daefa82399daf6
rauleite <raul.abreu.leite@gmail.com>
2025-06-14 21:25:50 -0300

    feat(tmuxp): ambiente e configurações de startup de sessões

---
+++ tmux/layout.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# ATENÇÃO: DEIXAR NO MESMO DIRETÓRIO DA CONFIGURAÇÃO DO TMUXP
+# (ou mudar o diretório dentro do arquivo de configuração - yaml no caso)
+# Seleciona a janela "editor" da sessão "ambiente_dev"
+tmux select-window -t ambiente_dev:editor
+# sleep 0.2
+
+# 1. Dividir horizontalmente: cria o painel à direita ocupando 50% da largura.
+tmux split-window -h -l 50% zsh
+# sleep 0.2
+
+# 2. Seleciona o painel esquerdo e divide verticalmente (para ter dois painéis na coluna esquerda)
+tmux select-pane -t 0
+# sleep 0.2
+tmux split-window -v -l 50% "tail -f /var/log/syslog"
+# sleep 0.2
+
+# 3. No painel superior (esquerdo) envia o comando para iniciar o nvim.
+tmux send-keys -t 0 "nvim ." Enter
